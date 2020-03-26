b3e4c802c48af64c2cae806619b2ecdcf0bf3a98
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-03-26 18:07:30 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,13 +26,13 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash[key] = a_value
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
- if hash[key]
+ if hash[key] ? 
    hash[key] += 1 
- else 
+   :
    hash[key] = 1
 end
 hash
